709a425348ba5d0f74efd607941e8ff272e80391
leadlockai407-oss <leadlockai407@gmail.com>
2026-02-06 05:53:01 -0500

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Homepage() {
+  return (
+      <main style={{ padding: 40 }}>
+        <h1>Lead Lock is live!</h1>
+        <p>Your site is deploying correctly</p>
+      </main>
+    );
+}
